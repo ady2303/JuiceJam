e14bd1fb166d94285af69b5f568bce45194fd4ff
ady2303 <2303singh.aditya@gmail.com>
2023-01-30 01:48:11 -0500

    Switched movement to follow mouse

--- JuiceJamGame/Scripts/Player.gd
+++ JuiceJamGame/Scripts/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 
-var speed = 200
+var speed = 250
 var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
@@ -9,16 +9,14 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	velocity = Vector2()
+	look_at_mouse()
+	move_to_mouse()
 	
-	if Input.is_action_pressed("down"):
-		velocity.y += speed
-	if Input.is_action_pressed("up"):
-		velocity.y -= speed
-	if Input.is_action_pressed("right"):
-		velocity.x += speed
-	if Input.is_action_pressed("left"):
-		velocity.x -= speed
-	var move_vel = velocity.normalized()
-	move_and_slide(speed*move_vel)
+func look_at_mouse():
 	look_at(get_global_mouse_position())
+	rotation_degrees += 0
+
+func move_to_mouse():
+	var direction = get_global_mouse_position() - position
+	direction = direction.normalized()
+	move_and_slide(direction*speed)
